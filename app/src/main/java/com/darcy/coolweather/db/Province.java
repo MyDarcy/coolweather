238fab453450ac61d959ab6076f0c3dfa19422e7
csqiang <1352475424@qq.com>
2017-04-19 19:28:28 +0800

    pojo created and litepal configurated

---
+++ app/src/main/java/com/darcy/coolweather/db/Province.java
@@ -0,0 +1,40 @@
+package com.darcy.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by csqiang
+ * Created on 2017/4/19-19:18.
+ * Description:
+ */
+
+public class Province extends DataSupport {
+
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
